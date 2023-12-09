73a516ebe2907940cf6b3fb455b10eda2351bb04
Benjamin Plante <benplante99@gmail.com>
2023-12-09 02:29:04 -0500

    start of rewrite

--- src/main.c
+++ src/main.c
@@ -3,116 +3,103 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bplante <bplante@student.42.fr>            +#+  +:+       +#+        */
+/*   By: bplante/Walord <benplante99@gmail.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/07 12:49:27 by bplante           #+#    #+#             */
-/*   Updated: 2023/11/22 15:48:11 by bplante          ###   ########.fr       */
+/*   Created: 2023/12/09 00:04:31 by bplante/Wal       #+#    #+#             */
+/*   Updated: 2023/12/09 02:27:24 by bplante/Wal      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <fcntl.h>
+#include <libft.h>
 #include "pipex.h"
 
-static int	run(char **argv, int io_files[2], char **env);
-static int	create_children(char *exec, char **env, t_pipe_pair *pipe_pair,
-				int extra_close[2]);
-static int	parent(pid_t pid);
-static void	children(char **exec, char **env, t_pipe_pair *pipe_pair,
-				int extra_close[2]);
+#define READ 0
+#define WRITE 1
+#define NOT_PIPE -2
+
+typedef struct s_pipes_tab
+{
+	int	len;
+	int	*pipes;
+}		t_pipe_tab;
 
 int	main(int argc, char **argv, char **env)
 {
-	int	io_files[2];
-	int	status;
+	t_pipe_tab	pipe_tab;
 
-	if (argc != 5)
+	if (argc > 5)
+		return (1);
+	pipe_tab.pipes = ft_calloc((argc - 3) * 2 + 1, sizeof(int));
+	pipe_tab.len = argc - 3;
+	pipe_tab.pipes[0 + READ] = open(argv[1], O_RDONLY | __O_CLOEXEC);
+	pipe_tab.pipes[0 + WRITE] = NOT_PIPE;
+	pipe_tab.pipes[(pipe_tab.len - 1) * 2 + WRITE] = open(argv[argc - 1],
+			O_WRONLY | O_CREAT | O_TRUNC | __O_CLOEXEC, 644);
+	pipe_tab.pipes[(pipe_tab.len - 1) * 2 + READ] = -2;
+	if (generate_pipes(&pipe_tab) == -1)
 	{
-		ft_printf_fd(
-			"pipex: usage <in_file> \"<command>\" \"<command>\" <out_file>\n",
-			2);
+		free(pipe_tab.pipes);
 		return (1);
 	}
-	if (check_io_files(argv[1], argv[argc - 1], io_files) == 0)
-	{
-		status = run(&argv[2], io_files, env);
-		close(io_files[1]);
-		close(io_files[0]);
-		return (status);
-	}
-	return (1);
 }
 
-int	run(char **argv, int io_files[2], char **env)
+int	fork_cmd(char *cmd, int *pipes, int pos, char **env)
 {
-	t_pipe_pair	*pipe_pair;
-	int			fd_pipe[2];
-	int			status;
+}
 
-	if (pipe(fd_pipe) != 0)
+int	generate_pipes(t_pipe_tab *pipes_tab)
+{
+	int	i;
+
+	i = 1;
+	while (i < pipes_tab->len - 1)
 	{
-		ft_printf_fd("pipex: pipe: %s\n", 2, strerror(errno));
+		if (pipe(&pipes_tab->pipes[i * 2]) == -1)
+			break ;
+		i++;
+	}
+	if (i != pipes_tab->len - 1)
+	{
+		i--;
+		while (i >= 0)
+		{
+			close(pipes_tab->pipes[i * 2 + WRITE]);
+			close(pipes_tab->pipes[i * 2 + READ]);
+			i--;
+		}
+	}
+	if (i == 0)
 		return (-1);
-	}
-	pipe_pair = create_pipe_pair(io_files[0], io_files[1], fd_pipe[0],
-			fd_pipe[1]);
-	status = create_children(argv[0], env, pipe_pair, NULL);
-	if (status != 0)
-	{
-		close_and_free(fd_pipe, pipe_pair);
-		return (status);
-	}
-	free(pipe_pair);
-	pipe_pair = create_pipe_pair(fd_pipe[0], fd_pipe[1], io_files[0],
-			io_files[1]);
-	status = create_children(argv[1], env, pipe_pair, NULL);
-	close_and_free(fd_pipe, pipe_pair);
-	return (status);
-}
-
-int	create_children(char *exec, char **env, t_pipe_pair *pipe_pair,
-		int extra_close[2])
-{
-	pid_t	pid;
-	char	**split;
-
-	pid = fork();
-	if (pid < 0)
-		ft_printf_fd("pipex: failed to fork: %s\n", 2, strerror(errno));
-	else if (pid == 0)
-	{
-		split = split_args(exec);
-		if (check_exec(split, env) == 0)
-			children(split, env, pipe_pair, extra_close);
-		free_tab((void **)split, &free);
-		close_pipes(pipe_pair->read, pipe_pair->write);
-		free(pipe_pair);
-		exit(1);
-	}
 	else
-	{
-		close(pipe_pair->write[1]);
-		return (parent(pid));
-	}
-	return (1);
+		return (0);
 }
 
-void	children(char **exec, char **env, t_pipe_pair *pipe_pair,
-		int extra_close[2])
+void	close_pipes(t_pipe_tab *pipe_tab, int pos)
 {
-	if (dup_close_pipes(pipe_pair, extra_close) == -1)
-	{
-		ft_printf_fd("pipex: piping error: %s\n", 2, strerror(errno));
-	}
-	else
-	{
-		execve(exec[0], exec, env);
-		ft_printf_fd("pipex: %s: %s\n", 2, strerror(errno), exec[0]);
-	}
-}
+	int	i;
 
-int	parent(pid_t pid)
-{
-	int	status;
-
-	waitpid(pid, &status, 0);
-	return (status / 256);
+	// close all real pipes not used by the cmd before
+	i = pos - 1;
+	while (i > 0)
+	{
+		close(pipe_tab->pipes[i * 2 + READ]);
+		close(pipe_tab->pipes[i * 2 + WRITE]);
+		i--;
+	}
+	if (pipe_tab->pipes[pos * 2 + WRITE] != NOT_PIPE)
+		close(pipe_tab->pipes[pos * 2 + WRITE]);
+	if (pipe_tab->pipes[(pos + 1) * 2 + READ] != NOT_PIPE)
+		close(pipe_tab->pipes[(pos + 1) * 2 + READ]);
+	if (pos != 0)
+		close(pipe_tab->pipes[0 + READ]);
+	if (pos != pipe_tab->len - 2)
+		close(pipe_tab->pipes[(pipe_tab->len - 1) * 2 + WRITE]);
+	i = pos + 2;
+	while (i < pipe_tab->len - 1)
+	{
+		close(pipe_tab->pipes[i * 2 + READ]);
+		close(pipe_tab->pipes[i * 2 + WRITE]);
+		i++;
+	}
 }
